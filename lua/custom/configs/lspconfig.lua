fd348a3d07ea186831476dd5bd5ef3cd41ed9c3f
marko154 <marko.gartnar777@gmail.com>
2023-10-15 23:52:48 +0200

    some cpp stuff i think

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -1,12 +1,16 @@
-local on_attach = function(client)
-  require("plugins.configs.lspconfig").on_attach(client)
-  -- require("illuminate").on_attach(client)
-end
+local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require("lspconfig")
 local util = require("lspconfig/util")
 
+
+-- Here are the languages that work:
+-- c
+-- c++ (sudo apt install libstdc++-dev (i think))
+-- js, ts, (import type breaks some things)
+-- golang, (maybe add debugger)
+-- python (still in works)
 
 lspconfig.gopls.setup {
   on_attach = on_attach,
@@ -20,16 +24,16 @@ lspconfig.gopls.setup {
   }
 }
 
-lspconfig["clangd"].setup {
-  on_attach = function(client)
+lspconfig.clangd.setup {
+  on_attach = function(client, bufnr)
     client.server_capabilities.signatureHelpProvider = false
-    on_attach(client)
+    on_attach(client, bufnr)
   end,
   capabilities = capabilities,
-  cmd = {
-    "clangd",
-    "--offset-encoding=utf-16",
-  },
+  -- cmd = {
+  --   "clangd",
+  --   "--offset-encoding=utf-16",
+  -- },
 }
 
 
